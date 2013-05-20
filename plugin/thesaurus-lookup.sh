f59aa001259f868cbc47f44efe1ddf7b9850e965
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-05-20 00:00:00 +0000

    Version 0.1.1: Fixed some Mac/Linux compatibility issues.

--- plugin/thesaurus-lookup.sh
+++ plugin/thesaurus-lookup.sh
@@ -2,7 +2,7 @@
 
 # Vim plugin for looking up words in an online thesaurus
 # Author:       Anton Beloglazov <http://beloglazov.info/>
-# Version:      0.1
+# Version:      0.1.1
 # Original idea and code: Nick Coleman <http://www.nickcoleman.org/>
 
 wget -q http://thesaurus.com/browse/${1} -O src.tmp
@@ -16,8 +16,13 @@ if cat src.tmp | grep 'queryn' > /dev/null; then
     echo -n 'Definition: '
     cat src.tmp | \
         grep -m2 -A1 'Definition' | \
-        sed -n -e 's/<td>\(.*\)<\/td.*/\1/p'
+        sed -n -e 's/<td>\(.*\)<\/td.*/\1/p' | \
+        sed 's/$/, /g' | \
+        tr -d '\n' | \
+        sed 's/, *$//' | \
+        tr -d '\n'
 
+    echo
     echo -n 'Synonyms: '
     cat src.tmp | \
         sed -n '/<td valign="top">Synonyms:<\/td>/,/\/span/ {/\/span/q; p;}' | \
