95b100b398e084d82413e77f5f18701f60ed6974
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-05-27 00:00:00 +0000

    Version 0.1.2: Replaced wget with curl (available on Mac OS X by default).

--- plugin/thesaurus-lookup.sh
+++ plugin/thesaurus-lookup.sh
@@ -2,10 +2,10 @@
 
 # Vim plugin for looking up words in an online thesaurus
 # Author:       Anton Beloglazov <http://beloglazov.info/>
-# Version:      0.1.1
+# Version:      0.1.2
 # Original idea and code: Nick Coleman <http://www.nickcoleman.org/>
 
-wget -q http://thesaurus.com/browse/${1} -O src.tmp
+curl -o src.tmp http://thesaurus.com/browse/${1} >/dev/null 2>&1
 
 if cat src.tmp | grep 'queryn' > /dev/null; then
     echo -n 'Main entry: '
